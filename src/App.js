0b65749c5ca5e04343b8f3c5662377305b0d70ee
mariam guchmazashvili <mariami.guchmazashvili.1@btu.edu.ge>
2023-03-03 22:54:01 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Profile from './components/Profile';
+import "./App.css";
+
+function App() {
+
+  return (
+  <>
+  <div className='App'>
+    <header className='header'>
+      <img src="/GitHub-logo.png" className="g-logo" alt="github-logo"/>
+    </header>
+    <Profile/>
+  </div>
+  </>
+  )
+}
+export default App;
+
